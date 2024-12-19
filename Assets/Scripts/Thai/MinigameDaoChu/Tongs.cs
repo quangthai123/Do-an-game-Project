558c04a03c3e2162b84b699acac3f25685f80a43
quangthai123 <lequangthai117@gmail.com>
2024-12-19 22:38:48 +0700

    update first logic minigame dao chu

---
+++ Assets/Scripts/Thai/MinigameDaoChu/Tongs.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tongs : MonoBehaviour
+{
+    public bool canGoBack = false;
+    public float pickUpSpeedModifier { get; private set; }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<IItemType>() != null)
+        {
+            canGoBack = true;
+            if(collision.GetComponent<RacoonMovement>() != null)
+                collision.GetComponent<RacoonMovement>().SetBePickedUp(transform);
+            pickUpSpeedModifier = collision.GetComponent<IItemType>().GetItemPickUpSpeedModifier();
+        }
+    }
+    public float GetPickingUpItemSpeedModifier() => pickUpSpeedModifier;
+    public void DisableItemOnFinishPickUp()
+    {
+        foreach(Transform transf in transform)
+        {
+            if(transf.gameObject.activeInHierarchy)
+                transf.gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Thai/MinigameDaoChu/TongsHandler.cs
+++ Assets/Scripts/Thai/MinigameDaoChu/TongsHandler.cs
@@ -6,7 +6,8 @@ public class TongsHandler : MonoBehaviour
 {
     [SerializeField] private float maxRotateAngle;
     [SerializeField] private float maxPickUpTime = 2f;
-    [SerializeField] private float pickUpSpeed = 2f;
+    [SerializeField] private float basePickUpSpeed = 2f;
+    [SerializeField] private Vector3 tongsOriginalLocalPos;
 
     public float rotateSpeed;
     public bool canRotate = true;
@@ -14,22 +15,22 @@ public class TongsHandler : MonoBehaviour
     private Transform tongs;
     private float angle;
     private bool rotateRight = true;
-    private Vector3 tongsOriginalLocalPos;
     private bool isPickingUp = false;
     void Start()
     {
         GameManagerDaoChu.Instance.onPlayerTouchingAction += StartPickUp;
         tongs = transform.Find("tongs");
-        tongsOriginalLocalPos = tongs.localPosition;
     }
     void FixedUpdate()
     {
-         RotateTongs();
+        RotateTongs();
     }
     private void StartPickUp()
     {
         if (isPickingUp)
             return;
+        tongsOriginalLocalPos = tongs.localPosition;
+        tongs.GetComponent<Tongs>().canGoBack = false;
         isPickingUp = true;
         canRotate = !canRotate;
         StartCoroutine("StartMoveToPickUp");
@@ -37,37 +38,71 @@ public class TongsHandler : MonoBehaviour
     private IEnumerator StartMoveToPickUp()
     {
         float pickUpTimer = maxPickUpTime;
-        while(pickUpTimer > 0f)
-        {
-            yield return new WaitForFixedUpdate();
-            tongs.localPosition += (-transform.up * pickUpSpeed);
-            pickUpTimer -= Time.fixedDeltaTime;
-        }
-        StartCoroutine("StartMoveBackAfterPickUp");
-    }
-    private IEnumerator StartMoveBackAfterPickUp()
-    {
-        float pickUpTimer = maxPickUpTime/2f;
+        Rigidbody2D tongsRb = tongs.GetComponent<Rigidbody2D>();
         while (pickUpTimer > 0f)
         {
             yield return new WaitForFixedUpdate();
-            tongs.localPosition += (transform.up * pickUpSpeed*2f);
+            tongsRb.velocity = -transform.up * basePickUpSpeed;
+            pickUpTimer -= Time.fixedDeltaTime;
+            if (tongs.GetComponent<Tongs>().canGoBack)
+            {
+                Debug.Log("Bat dau keo len!");
+                tongsRb.velocity = Vector3.zero;
+                StartCoroutine(StartMoveBackAfterCollided());
+                break;
+            }
+        }
+        if (!tongs.GetComponent<Tongs>().canGoBack)
+            StartCoroutine("StartMoveBackAfterPickUp");
+    }
+    private IEnumerator StartMoveBackAfterPickUp()
+    {
+        float pickUpTimer = maxPickUpTime / 2f;
+        Rigidbody2D tongsRb = tongs.GetComponent<Rigidbody2D>();
+        while (pickUpTimer > 0f)
+        {
+            yield return new WaitForFixedUpdate();
+            tongsRb.velocity = transform.up * basePickUpSpeed * 2f;
             pickUpTimer -= Time.fixedDeltaTime;
         }
+        tongs.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         tongs.localPosition = tongsOriginalLocalPos;
         canRotate = true;
         isPickingUp = false;
     }
+    private IEnumerator StartMoveBackAfterCollided()
+    {
+        float speedModifier = tongs.GetComponent<Tongs>().GetPickingUpItemSpeedModifier();
+        float pickUpTimer = maxPickUpTime / speedModifier;
+        Rigidbody2D tongsRb = tongs.GetComponent<Rigidbody2D>();
+        while (pickUpTimer > 0f)
+        {
+            yield return new WaitForFixedUpdate();
+            tongsRb.velocity = transform.up * basePickUpSpeed * speedModifier;
+            pickUpTimer -= Time.fixedDeltaTime;
+            if (Vector2.Distance(tongsOriginalLocalPos, tongs.localPosition) < .1f)
+            {
+                Debug.Log("Stop Pull!");
+                tongs.localPosition = tongsOriginalLocalPos;
+                tongsRb.velocity = Vector3.zero;
+                break;
+            }
+        }
+        canRotate = true;
+        isPickingUp = false;
+        tongs.GetComponent<Tongs>().DisableItemOnFinishPickUp();
+    }
     private void RotateTongs()
     {
-        if(!canRotate)
+        if (!canRotate)
             return;
         if (angle > maxRotateAngle - 1f && rotateRight)
         {
             rotateRight = false;
             maxRotateAngle = -maxRotateAngle;
             Debug.Log("Change Direction");
-        } else if(angle < maxRotateAngle + 1f && !rotateRight)
+        }
+        else if (angle < maxRotateAngle + 1f && !rotateRight)
         {
             rotateRight = true;
             maxRotateAngle = -maxRotateAngle;
@@ -77,3 +112,4 @@ public class TongsHandler : MonoBehaviour
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
+
